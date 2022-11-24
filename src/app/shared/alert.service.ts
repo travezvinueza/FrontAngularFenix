e7c5fb17551d01ae892ca4014522ffd02549ad99
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2022-11-23 21:47:00 -0500

    fix gitignore

--- src/app/shared/alert.service.ts
+++ src/app/shared/alert.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { FuseAlertType } from '@fuse/components/alert';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
+
+  alert: { type: FuseAlertType; message: string } = {
+    type: 'success',
+    message: ''
+  };
+  showAlert: boolean = false;
 
   constructor() { }
 }
