08a08344b7bf1f864721755d044a0fb401b6d9a6
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2022-11-29 16:05:33 -0500

    fix node js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ let express = require('express')
 
 let app = express()
 
+app.use(express.static(__dirname + '/dist/fuse'));
+
+
 app.use('/*',(req, resp) => {
     resp.sendFile(__dirname+'/dist/fuse/index.html')
 });
