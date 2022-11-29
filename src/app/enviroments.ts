c2a98d29902a3af203b1954988c5fb4f9b19c014
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2022-11-29 16:16:36 -0500

    enviroments

--- src/app/enviroments.ts
+++ src/app/enviroments.ts
@@ -1,3 +1,4 @@
 export class AppSettings{
-    public static API_PATH = "http://localhost:8080/api"
+    //public static API_PATH = "http://localhost:8080/api"
+    public static API_PATH = "https://app-tereas-backend.herokuapp.com/api"
 }
