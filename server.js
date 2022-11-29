a2378ba0565c925ee8f5b74df93251fc9f580d14
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2022-11-29 14:40:36 -0500

    fixes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/task-service-front'));
+app.use(express.static('./dist/fuse-angular'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/fuse/'}),
